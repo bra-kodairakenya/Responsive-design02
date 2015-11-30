cc52b42ff59373ed1b2539f32e71ea90c54d0571
Braian <kenyanke.1@gmail.com>
2015-11-30 16:47:59 +0900

    Create Mobile Header Navbar

---
+++ navbar.js
@@ -0,0 +1,16 @@
+$(function(){
+  $(".toggle").click(function(){
+    $(".menu").slideToggle(500);
+    return false;
+  });
+
+  $(window).resize(function(){
+    var win = $(window).width();
+    var size = 767;
+    if(win > size){
+      $(".menu").show();
+    } else {
+      $(".menu").hide();
+    }
+  });
+});
